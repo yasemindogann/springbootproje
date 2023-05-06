548025d60c898a4c05c9a7078281e5e7e017c354
Yasemin DOĞAN <yasemin.dn7@gmail.com>
2023-05-06 19:15:22 +0300

    v3

--- src/main/java/com/tpe/controller/StudentController.java
+++ src/main/java/com/tpe/controller/StudentController.java
@@ -1,11 +1,14 @@
 package com.tpe.controller;
 
+import com.tpe.domain.Student;
 import com.tpe.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController //Biz View kullanmıyoruz.RestAPI yapıyoruz.Status code(http) + response döneceği için böyle yaptık. Rest mimarisine uygun özelleştirilmiş bir controller demek istedim
 //Rest mimarisinin bize sağladığı imkanları kullanabileceğimiz bir Controller Classı olduğunu işaret ettik.
@@ -25,10 +28,31 @@ public class StudentController {
     //Endpoint'leri oluşturacağız, Postman'den yazdığımız methodlarımız çalışıyor mu diye test edicez.
 
     //Bütün öğrencileri getirelim.
-//    @GetMapping()    //endpoint + http methodları gidecek
-//    public ResponseEntity<Student>
+    //http://localhost:8080/students + GET  ==> Bu endpoint ile bana bir request gelirse ve bu da @GetMapping() ise bu methoda dallan demiş oluyorum.
+    @GetMapping()   //http://localhost:8080/students + GET    //endpoint + http methodları gidecek
+    //public Student getAll()           ==> Birden fazla öğrenci gelecek.
+    //public Student[] getAll()         ==> Kaç tane öğrenci gelecek bilmiyorum, esnek bir yapı lazım.
+    //public List<Student> getAll()     ==> StatusCode + Students gelecek List yeterli değil
+    //public Map<Student, ...> getAll() ==> Burayı Core Java ile(mesela Map) yapabilirim ama ...
+    //Spring Boot bizi düşünür.Madem sen Restfull API yapıyorsun Entity Class'ların dışında sen StatusCode da göndereceksin
+    //o zaman bu işi kolay bir şekilde yap diye ResponseEntity isminde bir Class yapısı gönderiyorum bana bu yapı ile bu kodları gönderebilirsin diyor.
+    public ResponseEntity<List<Student>> getAll(){ //ResponseEntity : Entity + StatusCode gönderiyor client'a.
+        //Dönen değerim ResponseEntity olacak ve Student'ları göndercem.
+        //Benim tek görevim bu gelen request'i service'e göndermek.
+        //1-Yukarda enjekte ettiğim servise'i çağırıyorum.
+        //2-StudentService'deki Listeleme methodunu çağır.
+        //Listeleme olduğu için bana student'lar gelmeli.Bu yüzden List kullanıyorum.
+        //return olarak bana dönen değer yukarda belirttiğim gibi ResponseEntity olacak.
+        //Bu yüzden ResponseEntity'nin "ok()" methodunu kullanıyorum ve içine bana gelen "students"'ları veriyorum.
+        //ok() methodu ne yapıyor? Bize yukardaki List yapıdaki(List<Student>) Student'ları gönderiyor + HTTP StatusCode'lardan  200 gönderiyor.
+        //ok() karşılığı 200 dür.  List<Student> + HTTP.Status code = 200
+        List<Student> students = studentService.getAll();
+        return ResponseEntity.ok(students); //Entity(List<Student>) + HTTP.Status code
+        //Öğrenci yoksa içi boş JSON gelir.  ==> []
 
-    //Githupa ekledim
+        //Burdan sonrası StudentService'de
+    }
+
 
 
 }
--- src/main/java/com/tpe/service/StudentService.java
+++ src/main/java/com/tpe/service/StudentService.java
@@ -1,8 +1,11 @@
 package com.tpe.service;
 
+import com.tpe.domain.Student;
 import com.tpe.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service    //Service katmanı olduğunu söylüyorum.
 public class StudentService {
@@ -13,4 +16,23 @@ public class StudentService {
     private StudentRepository studentRepository;
 
 
+    //Burası benim Business logic tarafım.Kontrol yapmam gerekiyorsa kontrol yapıcam.
+    //Senaryomu düşünüyorum;
+    //Tabloya gidicem data varsa getir, yoksa zaten boş gelecek.O zaman bir kontrole gerek yok.
+    //Kontrole gerek yoksa Repository tarafına gitmem lazım.
+    public List<Student> getAll() {
+        //studentRepository bir interface ve JpaRepository interace'ini extend ediyor.
+        //studentRepository. dediğimde JpaRepository'nin bütün methodlarına ulaşabilirim.
+        //İhtiyacım olan Student'ları listelemek bu yüzden findAll() methodunu kullanıyorum.
+//        List<Student> students = studentRepository.findAll();  //Bana dönen ne List data type'ında Student'lar.
+//        return students;
+        return studentRepository.findAll(); //Kısaca bu şekilde yazıyorum.
+        //Repository'ye gitmedim bile.Eğer bumethodlara ulaşamasaydım StudentRepository'ye gidip kendim method yazacaktım.
+        //Ama buna gerek kalmadı çünkü springframework.data.jpa'dan extend edildi  StudentRepository'de.
+
+        //findAll  ==> Method içine girdiğimde  "List<T> findAll();" Dönen değer List data type'ında Jenerik yapı.
+        //Ben T'nin Student olacağını setledim.
+
+        //Artık listeleme methodumu Postman'den test edebilirim.
+    }
 }
